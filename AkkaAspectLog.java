913f2e778ebf713c541a6a2f75f568c73c377394
germanblanco <german.blanco@gmail.com>
2015-12-31 11:45:51 +0100

    it all works but for the adding in the map. move to Scala.

--- AkkaAspectLog.java
+++ AkkaAspectLog.java
@@ -1,12 +1,16 @@
 package gbbaspecttest;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 import net.logstash.logback.marker.Markers;
 import net.logstash.logback.marker.LogstashMarker;
+
+import java.util.Map;
 
 @Aspect
 public class AkkaAspectLog {
@@ -17,9 +21,12 @@ public class AkkaAspectLog {
             argNames = "map")
     public void appendEntriesPointcut(Object map) {}
 
-    @Before(value = "appendEntriesPointcut(map)", argNames = "jp,map")
-    public void appendEntriesHit(JoinPoint jp, Object map) {
-        System.out.println("Hit Append Entries pointcut.");
+    @Around(value = "appendEntriesPointcut(map)", argNames = "jp,map")
+    public Object appendEntriesHit(ProceedingJoinPoint jp, Object map) throws Throwable {
+        Map m = (Map<String, Object>)map;
+ //       m.put("new_message", "Hello World!");
+        Object[] array = new Object[]{map};
+        return jp.proceed(array);
     }
 /*
   @Pointcut(value = "execution (net.logstash.logback.marker.LogstashMarker "
