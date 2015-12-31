5fd41d6ebb9fb835b0a2c88637397131ee6dd5bd
germanblanco <german.blanco@gmail.com>
2015-12-31 10:25:55 +0100

    Adding a pointcut for a Log function ... getLogger so far.

--- AkkaAspectLog.java
+++ AkkaAspectLog.java
@@ -7,11 +7,20 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class AkkaAspectLog {
+
+  @Pointcut(value = "execution (* getLogger(..))")
+  public void loggerInfoPointcut() {}
+
+  @Before(value = "loggerInfoPointcut()")
+  public void loggerInfoHit() {
+    System.out.println("Hit Logger Info pointcut.");
+  }
+
   @Pointcut(value = "execution (* hello(..))")
   public void helloPointcut() {}
 
   @Before(value = "helloPointcut()")
-  public void helloHello() {
-    System.out.println("Yahoooo.");
+  public void helloHit() {
+    System.out.println("Hit hello pointcut.");
   }
 }
