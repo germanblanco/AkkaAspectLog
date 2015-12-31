f6935f6bb7677b6da1ce5b10e29b92e793e2b172
germanblanco <german.blanco@gmail.com>
2015-12-31 10:56:55 +0100

    Adding a pointcut in appendEntries.

--- AkkaAspectLog.java
+++ AkkaAspectLog.java
@@ -5,9 +5,33 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
+import net.logstash.logback.marker.Markers;
+import net.logstash.logback.marker.LogstashMarker;
+
 @Aspect
 public class AkkaAspectLog {
 
+    @Pointcut(value = "execution ( * "
+            + "appendEntries(..)) "
+            + " ")
+    public void appendEntriesPointcut() {}
+
+    @Before(value = "appendEntriesPointcut()")
+    public void appendEntriesHit() {
+        System.out.println("Hit Append Entries pointcut.");
+    }
+/*
+  @Pointcut(value = "execution (net.logstash.logback.marker.LogstashMarker "
+                                   + "net.logstash.logback.marker.Markers.appendEntries(map)) "
+                                   + "&& args(map)",
+            argNames = "map")
+  public void appendEntriesPointcut(Object map) {}
+
+  @Before(value = "appendEntriesPointcut(map)", argNames = "jp,map")
+  public void appendEntriesHit(JoinPoint jp, Object map) {
+    System.out.println("Hit Append Entries pointcut.");
+  }
+*/
   @Pointcut(value = "execution (* getLogger(..))")
   public void loggerInfoPointcut() {}
 
